8cc6b6df72c62aed41542f00fe33229e003de91a
Mike Molenda <molenda.mike@gmail.com>
2017-05-06 15:13:26 -0500

    Extract chapter names

--- src/read_file_test.py
+++ src/read_file_test.py
@@ -1,6 +1,6 @@
 moby = []
 
-with open('../data/text/moby_dick_01.txt', newline='') as text:
+with open('../data/text/moby_dick.txt', newline='') as text:
         for line in text:
             moby.append(line.rstrip())  # strip double newlines
 
@@ -9,5 +9,10 @@ with open('../data/text/moby_dick_01.txt', newline='') as text:
 
 # Print only even lines
 # Demo that text file read as array of strings
-for line in range(0, len(moby), 2):
-    print(moby[line])
+# for line in range(0, len(moby), 2):
+#     print(moby[line])
+
+# extract chapter names
+for line in moby:
+    if line.upper().startswith('CHAPTER'):
+        print(line)
