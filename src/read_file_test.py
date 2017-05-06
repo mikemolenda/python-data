5662cf200b05f9bda833e028fdedb142aa636201
Mike Molenda <molenda.mike@gmail.com>
2017-05-06 15:03:08 -0500

    Print only even lines

--- src/read_file_test.py
+++ src/read_file_test.py
@@ -4,5 +4,10 @@ with open('../data/text/moby_dick_01.txt', newline='') as text:
         for line in text:
             moby.append(line.rstrip())  # strip double newlines
 
-for line in moby:
-    print(line)
+# for line in moby:
+#     print(line)
+
+# Print only even lines
+# Demo that text file read as array of strings
+for line in range(0, len(moby), 2):
+    print(moby[line])
